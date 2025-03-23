e57e16735828672e2c4e886226914bcf6d48a6a3
ellimckinley <190907228+ellimckinley@users.noreply.github.com>
2025-03-23 15:21:14 -0600

    created Candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,22 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    name: string;
+    email: string;
+    company: string;
+    location: string;
+    bio: string;
+    avatar_url: string;
+    repos_url: string;
+}
+
+// export default interface Candidate {
+//     login: string;
+//     avatar_url: string;
+//     // location:;
+//     // email:;
+//     // company:;
+//     // Bio:;
+//     repos_url: string;
+//     user_view_type: string;
+
+// }
